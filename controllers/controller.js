73db9f2643000ceb65604929b68e1eefd8408477
Brandon Sears <brandonsears8@gmail.com>
2018-03-27 14:41:07 -0400

    create add store and add product routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -22,11 +22,49 @@ router.get('/:id', (req, res) => {
     console.log(err)
   })
 })
+router.post('/', (req, res) => {
+  Store.create({
+    name: req.body.name,
+    about: req.body.about,
+    products: []
+  })
+  .then(store => {
+    res.json(`Store Created: ${store}`)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
 
 router.get('/:storeId/:productId', (req, res) => {
   Product.findOne({_id: req.params.productId})
   .then(product => {
     res.json(product)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
+router.post('/:id', (req, res) => {
+  Store.findOne({_id: req.params.id})
+  .then(store => {
+    Product.create({
+      artist: req.body.artist,
+      title: req.body.title,
+      description: req.body.description,
+      image: req.body.image,
+      price: req.body.price
+    })
+    .then(product => {
+      store.products.push(product)
+    })
+    .then(() => {
+      store.save()
+    })
+  })
+  .then(() => {
+    res.json('Product added')
   })
   .catch(err => {
     console.log(err)
