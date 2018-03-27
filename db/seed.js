a73b385ac61622f22d209761c54ffd5b43ef8528
Brandon Sears <brandonsears8@gmail.com>
2018-03-27 12:08:44 -0400

    add a store and product to seed

--- db/seed.js
+++ db/seed.js
@@ -5,7 +5,21 @@ Store.remove({}).then(() => {
   Product.remove({}).then(() => {
     Store.create({
       name: "Brandon's Store",
-      about: "Brandon's Store"
+      about: "Brandon's Cool Store"
+    }).then(store => {
+      Promise.all([
+        Product.create({
+          artist: 'Brandon',
+          tile: 'A dog',
+          description: 'a drawing of a dog',
+          image: 'https://i.pinimg.com/originals/5f/c8/37/5fc83789f0886812ef3f26e8a22823d2.jpg',
+          price: 500000
+        }).then(product => {
+          store.products.push(product)
+        })
+      ]).then(() => {
+        store.save(error => console.log(error))
+      })
     })
   })
 })
