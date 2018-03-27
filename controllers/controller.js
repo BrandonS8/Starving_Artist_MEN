d087c76ca92471c0568d54d6a8770c20d7c67569
Brandon Sears <brandonsears8@gmail.com>
2018-03-27 14:23:38 -0400

    create store id and product id routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -13,5 +13,24 @@ router.get('/', (req, res) => {
       })
 })
 
+router.get('/:id', (req, res) => {
+  Store.findOne({ _id: req.params.id})
+  .then(store => {
+    res.json(store)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
+router.get('/:storeId/:productId', (req, res) => {
+  Product.findOne({_id: req.params.productId})
+  .then(product => {
+    res.json(product)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
 
 module.exports = router
