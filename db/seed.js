c7bc7dbe6af7981647eb128ae123806a7b74910c
Brandon Sears <brandonsears8@gmail.com>
2018-03-27 12:21:45 -0400

    add another store to seed, fix comma in store model

--- db/seed.js
+++ db/seed.js
@@ -30,5 +30,32 @@ Store.remove({}).then(() => {
         store.save(error => console.log(error))
       })
     })
+    Store.create({
+      name: "Carl's Store",
+      about: "Carl's Lame Store"
+    }).then(store => {
+      Promise.all([
+        Product.create({
+          artist: 'Carl',
+          title: 'Carl',
+          description: 'A picture of carl',
+          image: 'https://vignette.wikia.nocookie.net/jimmyneutron/images/7/71/Carl.png/revision/latest?cb=20180219044646',
+          price: 1
+        }).then(product => {
+          store.products.push(product)
+        }),
+        Product.create({
+          artist: 'Carl',
+          title: 'Teacher Carl',
+          description: 'Carl does a teach',
+          image: 'https://vignette.wikia.nocookie.net/poohadventures/images/d/df/Carl_Wheezer.jpg/revision/latest?cb=20130526014202',
+          price: 2
+        }).then(product => {
+          store.products.push(product)
+        })
+      ]).then(() => {
+        store.save(error => console.log(error))
+      })
+    })
   })
 })
